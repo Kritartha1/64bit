925961d1b9f121294f00b6bd0a505effdb2a1d90
Anshu Suroliya <2000108444@hexaware.com>
2024-05-16 11:44:07 +0530

    o

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,19 +2,23 @@ import { Link } from "react-router-dom";
 
 const Content = () => {
     return (
-        <div className="w-full p-4 sm:p-8 flex flex-col sm:flex-row mt-12 sm:mt-24">
-            <div className="w-full sm:w-4/12 sm:ml-32 mb-8 sm:mb-0">
-                <div className="text-2xl sm:text-4xl text-white">Schedule your mock interview with InterviewAI today</div>
-                <div className="flex flex-col sm:flex-row mt-4 sm:mt-6">
-                    <Link className="px-6 sm:px-8 py-2 rounded-full text-white text-lg bg-gradient-to-r from-[#28528d] to-[#31cfe2] hover:from-[#023a89] hover:to-[#31cfe2] mb-4 sm:mb-0 sm:mr-6" to="/schedule">Start Mock</Link>
-                    <button className="px-6 py-2 rounded-full text-white text-lg border-white border-2">Join Interview</button>
+        <div className="w-full p-8 flex flex-col md:flex-row mt-24 items-center md:items-start">
+            <div className="w-full md:w-6/12 lg:w-4/12 ml-0 md:ml-44 text-center md:text-left">
+                <div className="text-4xl text-white">Schedule your mock interview with InterviewAI today</div>
+                <div className="flex mt-6 justify-center md:justify-start">
+                    <Link className="px-8 py-2 rounded-full text-white text-lg bg-gradient-to-r from-[#28528d] to-[#31cfe2] hover:from-[#023a89] to-[#31cfe2]" to="/schedule">
+                        Start Mock
+                    </Link>
+                    <button className="ml-4 md:ml-12 px-6 py-2 rounded-full text-white text-lg border-white border-2">
+                        Join Interview
+                    </button>
                 </div>
             </div>
-            <div className="flex justify-center sm:justify-start w-full sm:w-auto">
-                <img src="https://th.bing.com/th/id/OIP.Kxo6IQfCQCd1I-1FwAYRvwAAAA?rs=1&pid=ImgDetMain" className="rounded-xl sm:ml-12" alt="Interview" />
+            <div className="w-full md:w-6/12 lg:w-auto mt-8 md:mt-0 md:ml-12 flex justify-center md:justify-end">
+                <img src="https://th.bing.com/th/id/OIP.Kxo6IQfCQCd1I-1FwAYRvwAAAA?rs=1&pid=ImgDetMain" className="rounded-xl" alt="Interview AI" />
             </div>
         </div>
     );
-}
+};
 
 export default Content;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const Home=()=>{
     return(
-        <div className="w-full min-h-screen bg-gradient-to-br from-[#282f54] to-[#422f66]">
+        <div className="w-full min-h-screen bg-gradient-to-br overflow-x-hidden from-[#282f54] to-[#422f66]">
             <Navbar/>
             <Content/>
         </div>
