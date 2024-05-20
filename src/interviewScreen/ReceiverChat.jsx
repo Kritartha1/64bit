92a27e95fe09c4a19fc1ba65ccd75a3b11dd84b2
Kritartha1 <kritarthanathofficial@gmail.com>
2024-05-20 06:28:06 +0000

    Color of chats

--- src/interviewScreen/ReceiverChat.jsx
+++ src/interviewScreen/ReceiverChat.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ReceiverChat=({Text})=> {
   return (
-    <div className='bg-[#cff0cf] relative p-3.5 bg-white ml-2 mr-7 my-1 rounded flex self-start ' style={{overflowWrap:"anywhere"}}>
+    <div className='bg-white relative p-3.5 ml-2 mr-7 my-1 rounded flex self-start ' style={{overflowWrap:"anywhere"}}>
               {Text}
               <div className='absolute h-full w-2  bg-[#2A2D3F]' style={{left: "-3px",
     borderRadius: "19px",
--- src/interviewScreen/SenderChat.jsx
+++ src/interviewScreen/SenderChat.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const SenderChat=({Text})=> {
   return (
-    <div className='bg-[#cff0cf] relative p-3.5 bg-white mr-2 ml-7 my-1 rounded flex self-end ' style={{overflowWrap:"anywhere"}}>
+    <div className='bg-[#cff0cf] relative p-3.5  mr-2 ml-7 my-1 rounded flex self-end ' style={{overflowWrap:"anywhere"}}>
               {Text}
 
               <div className='absolute h-full w-2 bg-[#2A2D3F] ' style={{right: "-3px",borderRadius: "19px",top: "1px"}}
